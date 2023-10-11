7897d526cb0618809ba576620aff31eb05f832a1
Quennan Serbias T. Tunguia <tunguia.quennanserbias12@gmail.com>
2023-10-11 17:14:36 +0800

    Added Custom Filter

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,24 @@
-import { Hero, SearchBar, CustomFilter} from '@/components'
+import { Hero, SearchBar, CustomFilter, CarCard} from '@/components'
+import { fuels, yearsOfProduction } from '@/constants';
+import { fetchCars } from '@/utils'
 import { Herr_Von_Muellerhoff } from 'next/font/google'
 import Image from 'next/image'
 
-export default function Home() {
+
+export default async function Home({searchParams}) {
+
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer,
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || '',
+    limit: searchParams.limit || 10,
+    model: searchParams.model || '',
+  });
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars; 
+
+
+
   return (
     <main className="overflow-hidden">
       <Hero/>
@@ -17,10 +33,25 @@ export default function Home() {
           <SearchBar />
 
           <div className="home__filter-container"> 
-            <CustomFilter title="fuel"/>
-            <CustomFilter title="year"/>
+            <CustomFilter title="fuel" options={fuels}/>
+            <CustomFilter title="year" options={yearsOfProduction}/>
           </div>
         </div>
+
+        {
+          !isDataEmpty ? (
+            <section>
+             <div className = "home__cars-wrapper">
+              {allCars?.map((car) => <CarCard car={car}/>)}
+             </div>
+            </section>
+          ) : (
+            <div className="home__error-container">
+              <h2 className="text-black text-xl font-bold">Ooops, no results</h2>
+              <p>{allCars?.message}</p>
+            </div>
+          )
+        }
       </div>
     </main>
   )
--- components/index.ts
+++ components/index.ts
@@ -5,6 +5,8 @@ import Footer from './Footer'
 import SearchBar from './SearchBar'
 import CustomFilter from './CustomFilter'
 import SearchManufacturer from './SearchManufacturer'
+import CarCard from './CarCard'
+import CarDetails from './CarDetails'
 export{
     Hero,
     CustomButton,
@@ -12,5 +14,7 @@ export{
     Footer,
     SearchBar,
     CustomFilter,
-    SearchManufacturer
+    SearchManufacturer,
+    CarCard,
+    CarDetails
 }
